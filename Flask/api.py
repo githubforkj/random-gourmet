982c431a3456ff0a679abdb17c1a1d6851a478ff
githubforkj <105629315+githubforkj@users.noreply.github.com>
2022-09-19 00:52:42 +0900

    いくつかできた
    - 場所のラジオボタンをAPIのmiddle_areaと同様の名称に変更した
    - APIから[店ID、店名、場所、URL、画像、営業時間]を取得することに成功した
    - 上記のデータを場所で絞り込むことに成功した
        - 料金、人数では絞り込めていない。(人数での絞り込みは不要？)

--- Flask/api.py
+++ Flask/api.py
@@ -1,44 +1,90 @@
+from ast import get_source_segment
+from urllib import response
 import requests
 import json 
 
+# 中エリアを取得する
+def middle_area_masta():
+    # エリアマスタAPI用のクエリ
+    query = {
+    'key':'2bc4f479e6c2392d',
+    'keyword':'',
+    'large_area':'Z011', #　大エリアコードで東京を指定
+    'format':'json'
+    }
+    # エリアマスタAPIのリクエストURL
+    url = 'http://webservice.recruit.co.jp/hotpepper/middle_area/v1/'
+    # JSONを取得
+    responce = requests.get(url, query)
+    # JSONの読み込み
+    result = json.loads(responce.text)['results']['middle_area']
 
-# 検索クエリ
+    middle_area = {}
+    # 中エリアのコードと名称を取得
+    for (u,v) in enumerate(result):
+        # print(v['code'],'-',v['name'])
+        # print('"{}"'.format(v['name']),',')
+        # print('--------------------------------------------')
+        middle_area[v['name']] = v['code']
+    
+    return middle_area
 
-query = {
-    'key': '＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊',
-    'lat': '35.75012327488292', # 北千住駅の緯度
-    'lng': '139.80508505043517', # 北千住駅の経度
-    'range': '1',
-    'count': 50,
+# 店舗情報等を取得する
+# 場所のみ絞り込みが出来ている。後は価格帯を整形する。
+def gourmet_search(code):
+    # グルメサーチAPI用のクエリ
+    query = {
+    'key': '2bc4f479e6c2392d',
+    # 'lat': '35.75012327488292', # 北千住駅の緯度
+    # 'lng': '139.80508505043517', # 北千住駅の経度
+    'middle_area': code, # 中エリアのコードを入力する
+    'range': '3',
+    'count': 100,
     'format': 'json'
-}
+        }
+    #  グルメサーチAPIのリクエストURL
+    url = 'https://webservice.recruit.co.jp/hotpepper/gourmet/v1/'
+
+    # APIを使用し、JSONを取得
+    responce = requests.get(url, query)
+
+    # JSONを読み込む
+    result = json.loads(responce.text)['results']['shop']
+
+    gourment = []
+
+    # 店名、予算、中エリア、店舗URL、画像、営業時間の取得
+    for (u,v) in enumerate(result):
+        g = []
+        # 店idと店名
+        g.append(v['id'])
+        g.append(v['name'])
+        
+        # 予算とコード
+        g.append(v['budget']['name'])
+        g.append(v['budget']['code'])
+
+        # 中エリアとコード
+        g.append(v['middle_area']['name'])
+        g.append(v['middle_area']['code'])
+
+        # 店舗URL
+        g.append(v['urls']['pc'])
+
+        # 画像
+        g.append(v['photo']['pc']['l'])
+        g.append(v['photo']['mobile']['l'])
+
+        # 営業時間
+        g.append(v['open'])
+
+        gourment.append(g)
+
+    print('gourment')
+    return gourment
+    
+    
 
 
-# グルメサーチAPIのリクエストURL
-url1 = 'https://webservice.recruit.co.jp/hotpepper/gourmet/v1/'
-
-# URLとクエリでリクエスト
-responce = requests.get(url1, query)
-
-# 戻り値をjson形式で読み出し、['results']['shop']を抽出
-result = json.loads(responce.text)['results']['shop']
-
-# 'non_smoking'フィールドが'全面禁煙'以外の場合
-# 店名、'non_smoking'フィールド、ジャンルを表示
-for i in result:
-    print(i)
-    print('----------------------------------------------------------')
-    # if i['non_smoking'] !='全面禁煙':
-    #     print(
-    #         i['name'], '■',
-    #         i['non_smoking'],'■',
-    #         i['genre']['name']
-    #     )
-
-
-# エリアマスタAPIのリクエストURL
-url2 = http://webservice.recruit.co.jp/hotpepper/middle_area/v1/
-
-query = {
-    # ＊＊＊＊
-}
+middle_area_masta()
+# gourmet_search()
--- Flask/app.py
+++ Flask/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import random
+import api
 
 
 app = Flask(__name__)
@@ -14,23 +15,23 @@ class Attr(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, default=0)
     num = db.Column(db.Integer, default=0)
-    place = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+    place = db.Column(db.String(30), default=0)
+    area_id = db.Column(db.String(30), default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<record %r>' % self.id
 
 
-
-# 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         price = request.form.get('price')
         num = request.form.get('num')
         place = request.form.get('place')
+        api1 = api.middle_area_masta()
 
-        new_record = Attr(price=price,num=num,place=place)
+        new_record = Attr(price=price, num=num, place=place, area_id = api1[place])
 
         try:
             db.session.add(new_record)
@@ -41,14 +42,116 @@ def index():
 
     else:
         records = Attr.query.order_by(Attr.date_created).all()
-        return render_template('index.html', records = records)
-
-
+        codes = Attr.query.order_by(Attr.id.desc()).first()
+        if codes != None:
+            code = codes.area_id
+            api1 = api.middle_area_masta()
+            api2 = api.gourmet_search(code)
+            middle_area = ["銀座・有楽町・新橋・築地・月島",
+                    "水道橋・飯田橋・神楽坂",
+                    "お台場",
+                    "東京・大手町・日本橋・人形町",
+                    "四ツ谷・麹町・市ヶ谷・九段下",
+                    "上野・御徒町・浅草",
+                    "北千住・日暮里・葛飾・荒川",
+                    "錦糸町・浅草橋・両国・亀戸",
+                    "門前仲町・東陽町・木場・葛西",
+                    "神田・神保町・秋葉原・御茶ノ水",
+                    "品川･目黒･田町･浜松町･五反田",
+                    "蒲田・大森・大田区",
+                    "渋谷",
+                    "原宿・青山・表参道",
+                    "恵比寿・中目黒・代官山・広尾",
+                    "赤坂・六本木・麻布十番・西麻布",
+                    "自由が丘・田園調布",
+                    "池袋",
+                    "赤羽・王子・十条",
+                    "新宿",
+                    "新大久保・大久保",
+                    "巣鴨・大塚・駒込",
+                    "中野・高円寺・阿佐ヶ谷・方南町",
+                    "下北沢・代々木上原",
+                    "高田馬場",
+                    "池尻大橋・三軒茶屋・駒沢大学",
+                    "桜新町・用賀・二子玉川",
+                    "祐天寺・学芸大学・都立大学",
+                    "幡ヶ谷・笹塚・明大前・下高井戸",
+                    "調布・府中・千歳烏山・仙川",
+                    "経堂・千歳船橋",
+                    "祖師ヶ谷大蔵・成城学園前",
+                    "大井町･中延･旗の台･戸越･馬込",
+                    "不動前・武蔵小山",
+                    "雪が谷大塚・池上",
+                    "武蔵小金井",
+                    "国立・国分寺",
+                    "青梅・昭島・小作・青梅線沿線",
+                    "多摩センター・南大沢",
+                    "吉祥寺・荻窪・三鷹",
+                    "町田",
+                    "八王子・立川",
+                    "西武池袋線（石神井公園～秋津）",
+                    "西武新宿線(中井～田無～東村山)",
+                    "練馬・板橋・成増・江古田",
+                    "都営三田線（新板橋～西高島平）",
+                    "聖蹟桜ヶ丘・高幡不動・分倍河原",
+                    "東京都その他"]
+            return render_template('index.html', records=records, api1=api1, api2 = api2, middle_area = middle_area)
+        else:
+            middle_area = ["銀座・有楽町・新橋・築地・月島",
+                    "水道橋・飯田橋・神楽坂",
+                    "お台場",
+                    "東京・大手町・日本橋・人形町",
+                    "四ツ谷・麹町・市ヶ谷・九段下",
+                    "上野・御徒町・浅草",
+                    "北千住・日暮里・葛飾・荒川",
+                    "錦糸町・浅草橋・両国・亀戸",
+                    "門前仲町・東陽町・木場・葛西",
+                    "神田・神保町・秋葉原・御茶ノ水",
+                    "品川･目黒･田町･浜松町･五反田",
+                    "蒲田・大森・大田区",
+                    "渋谷",
+                    "原宿・青山・表参道",
+                    "恵比寿・中目黒・代官山・広尾",
+                    "赤坂・六本木・麻布十番・西麻布",
+                    "自由が丘・田園調布",
+                    "池袋",
+                    "赤羽・王子・十条",
+                    "新宿",
+                    "新大久保・大久保",
+                    "巣鴨・大塚・駒込",
+                    "中野・高円寺・阿佐ヶ谷・方南町",
+                    "下北沢・代々木上原",
+                    "高田馬場",
+                    "池尻大橋・三軒茶屋・駒沢大学",
+                    "桜新町・用賀・二子玉川",
+                    "祐天寺・学芸大学・都立大学",
+                    "幡ヶ谷・笹塚・明大前・下高井戸",
+                    "調布・府中・千歳烏山・仙川",
+                    "経堂・千歳船橋",
+                    "祖師ヶ谷大蔵・成城学園前",
+                    "大井町･中延･旗の台･戸越･馬込",
+                    "不動前・武蔵小山",
+                    "雪が谷大塚・池上",
+                    "武蔵小金井",
+                    "国立・国分寺",
+                    "青梅・昭島・小作・青梅線沿線",
+                    "多摩センター・南大沢",
+                    "吉祥寺・荻窪・三鷹",
+                    "町田",
+                    "八王子・立川",
+                    "西武池袋線（石神井公園～秋津）",
+                    "西武新宿線(中井～田無～東村山)",
+                    "練馬・板橋・成増・江古田",
+                    "都営三田線（新板橋～西高島平）",
+                    "聖蹟桜ヶ丘・高幡不動・分倍河原",
+                    "東京都その他"]
+            return render_template('index.html', middle_area=middle_area)
 
 
 def roulette(num):
     num = random.randrange(100)
     return num
+
 
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -62,7 +165,8 @@ def delete(id):
     except:
         return ('There was a problem deleting that task')
 
-@app.route('/update/<int:id>', methods = ['GET','POST'])
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     task = Attr.query.get_or_404(id)
     if request.method == 'POST':
@@ -74,7 +178,8 @@ def update(id):
         except:
             return 'There was an issue updating your task'
     else:
-        return render_template('update.html', task = task)
+        return render_template('update.html', task=task)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
